1a070ee59941794bae0f84846b907461a485e12d
namngovan1 <namngo0311@gmail.com>
2023-07-12 16:54:21 +0700

    nam sưa rooms

--- resources/views/frontend/details.blade.php
+++ resources/views/frontend/details.blade.php
@@ -1,54 +1,61 @@
+
 @extends('frontend.master')
 
 @section('content')
+<div class="row">
 
-<div class="container">
+  <div class="col-lg-12 margin-tb">
 
-<h1 class="title">Luxirious Suites</h1>
+      <div class="pull-left">
 
+          <h2> Show Rooms</h2>
 
+      </div>
 
-<!-- RoomDetails -->
-<div id="RoomDetails" class="carousel slide" data-ride="carousel">
-    <div class="carousel-inner">
-        <div class="item active"><img src="{{ asset('images/photos/8.jpg') }}" class="img-responsive" alt="slide"></div>
-        <div class="item height-full"><img src="{{ asset('images/photos/9.jpg') }}" class="img-responsive" alt="slide"></div>
-        <div class="item height-full"><img src="{{ asset('images/photos/10.jpg') }}" class="img-responsive" alt="slide"></div>
-    </div>
-    <!-- Controls -->
-    <a class="left carousel-control" href="#RoomDetails" role="button" data-slide="prev"><i class="fa fa-angle-left"></i></a>
-    <a class="right carousel-control" href="#RoomDetails" role="button" data-slide="next"><i class="fa fa-angle-right"></i></a>
-</div>
-<!-- RoomCarousel-->
+      <div class="pull-right">
 
+          <a class="btn btn-primary" href="{{ route('frontend.rooms') }}"> Back</a>
 
-<div class="room-features spacer">
-  <div class="row">
-    <div class="col-sm-12 col-md-5"> 
-    <p>Space in your house How to sell faster than your neighbors How to make a strategic use. To discourage you by telling. To discourage you by telling. I m going to outline 14 different ways that I ve found you. The real goal of any talk or speech. The real goal of any talk or speech. I m going to outline 14 different ways that I ve found you. The real goal of any talk or speech. I m going to outline 14 different ways that I ve found you. The real goal of any talk or speech. To discourage you by telling. To discourage you by telling. Space in your house How to sell faster than your neighbors How to make a strategic use. The real goal of any talk or speech.</p>
-    <p>By Learning Ways To Become Peaceful. One of the greatest barriers to making the sale is your prospect's natural. Don't stubbornly. Don't stubbornly. Don't stubbornly. -And Gain Power By Learning Ways To Become Peaceful. </p>
-    </div>
-    <div class="col-sm-6 col-md-3 amenitites"> 
-    <h3>Amenitites</h3>    
-    <ul>
-      <li>One of the greatest barriers to making the sale is your prospect.</li>
-      <li>Principle to work to make more money while having more fun.</li>
-      <li>Unlucky people. Don't stubbornly.</li>
-      <li>Principle to work to make more money while having more fun.</li>
-      <li>Space in your house How to sell faster than your neighbors</li>
-    </ul>
-    
+      </div>
 
-    </div>  
-    <div class="col-sm-3 col-md-2">
-      <div class="size-price">Size<span>44 sq</span></div>
-    </div>
-    <div class="col-sm-3 col-md-2">
-      <div class="size-price">Price<span>$200.00</span></div>
-    </div>
   </div>
+
 </div>
-                     
+
+<div class="row">
+
+  <div class="col-xs-12 col-sm-12 col-md-12">
+
+      <div class="form-group">
+
+          <strong>Name:</strong>
+
+          {{ $room->name }}
+
+      </div>
+
+  </div>
+
+
+  <div class="col-xs-12 col-sm-12 col-md-12">
+
+      <div class="form-group">
+
+          <strong>Image:</strong>
+         
+          @foreach ($product->image as $image)
+              <img src="{{ asset('image/rooms/' . $image->image) }}" alt="" style="margin: 15px" height=150
+                  width=250>
+          @endforeach
+
+
+
+
+      </div>
+
+  </div>
+
+
 
 
 </div>
--- resources/views/frontend/rooms.blade.php
+++ resources/views/frontend/rooms.blade.php
@@ -1,3 +1,127 @@
+<style>
+  .rooms-group {
+  margin-bottom: 20px;
+}
+
+.row-index {
+  display: flex;
+  flex-wrap: wrap;
+}
+
+.col-xs-18 {
+  width: 100%;
+}
+
+@media (min-width: 576px) {
+  .col-sm-6 {
+    flex: 0 0 50%;
+    max-width: 50%;
+  }
+}
+
+@media (min-width: 768px) {
+  .col-md-4-index {
+    flex: 0 0 33.333333%;
+    max-width: 33.333333%;
+  }
+}
+
+.img_thumbnail {
+  position: relative;
+}
+
+.carousel-inner {
+  position: relative;
+  width: 100%;
+  height: 0;
+  padding-bottom: 75%;
+  overflow: hidden;
+}
+
+.carousel-inner .carousel-item {
+  position: absolute;
+  top: 0;
+  left: 0;
+  width: 100%;
+  height: 100%;
+}
+
+.carousel-inner .carousel-item img {
+  width: 100%;
+  height: 100%;
+  object-fit: cover;
+}
+
+.carousel-control-prev,
+.carousel-control-next {
+  position: absolute;
+  top: 50%;
+  transform: translateY(-50%);
+  width: 30px;
+  height: 30px;
+  border-radius: 50%;
+  background-color: rgba(0, 0, 0, 0.5);
+  color: #fff;
+  text-align: center;
+  font-size: 20px;
+  line-height: 30px;
+  cursor: pointer;
+}
+
+.carousel-control-prev {
+  left: 10px;
+}
+
+.carousel-control-next {
+  right: 10px;
+}
+
+.caption {
+  padding: 10px;
+}
+
+.caption h3 {
+  margin-top: 0;
+  text-align: center;
+  font-weight: bold;
+  font-size: 18px;
+  color: black;
+}
+.caption h4 {
+  text-align: center;
+}
+
+.status-badge {
+  margin-top: 10px;
+  color: white;
+}
+
+.status-badge span {
+  display: inline-block;
+  padding: 5px 10px;
+  border-radius: 4px;
+}
+
+.status-badge .bg-warning-light {
+  background-color: rgb(112, 239, 83);
+  color: white;
+}
+
+.status-badge .bg-danger-light {
+  background-color: rgb(247, 70, 70);
+  color: white;
+}
+
+.status-badge .bg-info-light {
+  background-color: rgb(248, 248, 56);
+  color: black;
+}
+
+.purchase-info {
+  margin-top: 10px;
+}
+
+</style>
 @extends('frontend.master')
 
 @section('content')
@@ -9,27 +133,68 @@
 
 <!-- form -->
 
-<div class="row">
-      <div class="col-sm-6 wowload fadeInUp">
-        <div class="rooms">
-          <img src="images/photos/8.jpg" class="img-responsive">
-          <div class="info">
-            <h3>Phòng sang trọng</h3>
-            <p>...</p>
-            <a href="{{ url('/details') }}" class="btn btn-default">Xem chi tiết</a>
+  <div class="rooms-group">
+  <div class="row-index">
+    @foreach($rooms as $room)
+    <div class="col-xs-18 col-sm-6 col-md-4-index" >
+      <div class="img_thumbnail productlist-index">
+        <div id="carousel-{{ $room->id }}" class="carousel slide" data-ride="carousel">
+          <ol class="carousel-indicators">
+            @if ($room->picture) <li data-target="#carousel-{{ $room->id }}" data-slide-to="0" class="active"></li> @endif
+            @if ($room->image2) <li data-target="#carousel-{{ $room->id }}" data-slide-to="1"></li> @endif
+            @if ($room->image3) <li data-target="#carousel-{{ $room->id }}" data-slide-to="2"></li> @endif
+            @if ($room->image4) <li data-target="#carousel-{{ $room->id }}" data-slide-to="3"></li> @endif
+            @if ($room->image5) <li data-target="#carousel-{{ $room->id }}" data-slide-to="4"></li> @endif
+          </ol>
+          <div class="carousel-inner">
+            @if ($room->picture)
+            <div class="carousel-item active"><img src="{{ asset('images/rooms/' . $room->picture->file_name) }}" class="img-fluid rounded avatar-50 mr-3" alt="image"></div>
+            @endif
+            @if ($room->image2)
+            <div class="carousel-item"><img src="{{ asset('images/rooms/' . $room->image2) }}" class="img-fluid rounded avatar-50 mr-3" alt="image"></div>
+            @endif
+            @if ($room->image3)
+            <div class="carousel-item"><img src="{{ asset('images/rooms/' . $room->image3) }}" class="img-fluid rounded avatar-50 mr-3" alt="image"></div>
+            @endif
+            @if ($room->image4)
+            <div class="carousel-item"><img src="{{ asset('images/rooms/' . $room->image4) }}" class="img-fluid rounded avatar-50 mr-3" alt="image"></div>
+            @endif
+            @if ($room->image5)
+            <div class="carousel-item"><img src="{{ asset('images/rooms/' . $room->image5) }}" class="img-fluid rounded avatar-50 mr-3" alt="image"></div>
+            @endif
           </div>
+          <a class="carousel-control-prev" href="#carousel-{{ $room->id }}" role="button" data-slide="prev">
+            <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+            <span class="sr-only">Previous</span>
+          </a>
+          <a class="carousel-control-next" href="#carousel-{{ $room->id }}" role="button" data-slide="next">
+            <span class="carousel-control-next-icon" aria-hidden="true"></span>
+            <span class="sr-only">Next</span>
+          </a>
         </div>
       </div>
-      <div class="col-sm-6 wowload fadeInUp">
-        <div class="rooms">
-          <img src="images/photos/9.jpg" class="img-responsive">
-          <div class="info">
-            <h3>Phòng sang trọng</h3>
-            <p>...</p>
-            <a href="{{ url('/details') }}" class="btn btn-default">Xem chi tiết</a>
-          </div>
+      <div class="caption">
+        <h3>{{ $room->name }}</h3>
+        <h4>{{ $room->roomType->name }}</h4>
+        <strong>Status:</strong>
+        <div class="status-badge">
+          @if ($room->status === 'active')
+          <span class="badge bg-warning-light">Active</span>
+          @elseif ($room->status === 'maintenance')
+          <span class="badge bg-danger-light">Maintenance</span>
+          @elseif ($room->status === 'vacancy')
+          <span class="badge bg-info-light">Vacancy</span>
+          @endif
         </div>
+        <p class="purchase-info"><a href="" class="btn btn-primary btn-block text-center" role="button">Order</a></p>
+        <p class="purchase-info"><a href="{{ url('/details', $room->id) }}" class="btn btn-primary btn-block text-center" role="button">View Detail</a></p>
       </div>
+    </div>
+    @endforeach
+  </div>
+</div>
+
+      
 </div>
                      <div class="text-center">
                      <ul class="pagination">
